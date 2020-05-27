73081204eab0c9adda584455a1c67a32db052049
qudl-huohua <dlqu@huohuaschool.com>
2020-05-27 17:55:54 +0800

    处理js警告

--- src/footer/js/footer.js
+++ src/footer/js/footer.js
@@ -4,25 +4,28 @@ $(function(){
     function arrowRotate(num) {
         Object.keys(arrow).forEach(function(key) {
             arrow[key].style.transform = "scaleY("+ -1 +")";
-        })
+        });
         arrow[num-1].style.transform = "scaleY("+ 1 +")";
     }
-    $('#collapseOne').on('show.bs.collapse', function () {
+    let collapseOne =  $('#collapseOne');
+    collapseOne.on('show.bs.collapse', function () {
         arrowRotate(1);
-    })
-    $('#collapseOne').on('hide.bs.collapse', function () {
+    });
+    collapseOne.on('hide.bs.collapse', function () {
         arrow[0].style.transform = "scaleY("+ -1 +")";
-    })
-    $('#collapseTwo').on('show.bs.collapse', function () {
+    });
+    let collapseTwo =  $('#collapseTwo');
+    collapseTwo.on('show.bs.collapse', function () {
         arrowRotate(2);
-    })
-    $('#collapseTwo').on('hide.bs.collapse', function () {
+    });
+    collapseTwo.on('hide.bs.collapse', function () {
         arrow[1].style.transform = "scaleY("+ -1 +")";
-    })
-    $('#collapseThree').on('show.bs.collapse', function () {
+    });
+    let collapseThree =  $('#collapseThree');
+    collapseThree.on('show.bs.collapse', function () {
         arrowRotate(3);
-    })
-    $('#collapseThree').on('hide.bs.collapse', function () {
+    });
+    collapseThree.on('hide.bs.collapse', function () {
         arrow[2].style.transform = "scaleY("+ -1 +")";
-    })
+    });
 });
--- src/header/js/header.js
+++ src/header/js/header.js
@@ -5,17 +5,17 @@ window.onload = function() {
         //  鼠标移入时，将除自己外的项透明度改为40%
         navContent[key].getElementsByTagName("a")[0].onmouseover = function () {
             Object.keys(navContent).forEach(function(key2) {
-                navContent[key2].style.opacity = 0.4;
-            })
-            navContent[key].style.opacity = 1;
-        }
+                navContent[key2].style.opacity = '0.4';
+            });
+            navContent[key].style.opacity = '1';
+        };
         navContent[key].getElementsByTagName("a")[0].onmouseout = function () {
             //  鼠标移除时还原样式,透明度改回100%
             Object.keys(navContent).forEach(function (key) {
-                navContent[key].style.opacity = 1;
+                navContent[key].style.opacity = '1';
             });
         }
     })
-}
+};
 
 
