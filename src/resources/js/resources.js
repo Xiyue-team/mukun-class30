3513d2a9c48ef23e89ce5b55e90b89715f78e25b
gwli <1433365855@qq.com>
2020-06-18 15:31:10 +0800

    update：资源页面大图宽度修改,动态计算

--- src/resources/js/resources.js
+++ src/resources/js/resources.js
@@ -10,8 +10,12 @@ class ResourcesPage {
             let navContent = document.getElementsByClassName('nav-content');
             navContent[1].style.opacity = '1';
         },200)
+
+        this.imgWidth();
+
         // 窗口resize事件
         window.onresize = () => {
+            this.imgWidth();
             this.zoomImg();
         };
     }
@@ -19,43 +23,24 @@ class ResourcesPage {
     // 图片高度等比缩放
     zoomImg() {
         let width = document.body.clientWidth;
-        let detailImgs = document.getElementsByClassName('detail-img');
-        let detailWidth = document.getElementsByClassName('detail')[0].offsetWidth;
-        let backHeight, imgWidth,imgHeight;
+        let backHeight;
         if (width >= 1440) {
             backHeight = width * (800/1920);
-            // imgWidth = detailWidth * (497/628);
-            // imgHeight = imgWidth * (410/497);
-            // Object.keys(detailImgs).forEach(function(key){
-            //     detailImgs[key].style.width = imgWidth.toString() + 'px';
-            //     detailImgs[key].style.height = imgHeight.toString() + 'px';
-            // })
         } else if (width < 1440 && width >= 1069) {
             backHeight = width * (800/1280);
-            // imgWidth = detailImgs[0].offsetWidth;
-            // imgHeight = imgWidth * (410/414.6);
-            // Object.keys(detailImgs).forEach(function(key){
-            //     detailImgs[key].style.width = '100%';
-            //     detailImgs[key].style.height = imgHeight.toString() + 'px';
-            // })
         } else if (width < 1069 && width >= 735) {
             backHeight = width * (650/900);
-            // imgWidth = detailImgs[0].offsetWidth;
-            // Object.keys(detailImgs).forEach(function(key){
-            //     detailImgs[key].style.width = '100%';
-            //     detailImgs[key].style.height = imgWidth.toString() + 'px';
-            // })
         } else if (width < 735) {
             backHeight = width * (236/321);
-            // imgWidth = detailImgs[0].offsetWidth;
-            // imgHeight = imgWidth * (284/321);
-            // Object.keys(detailImgs).forEach(function(key){
-            //     detailImgs[key].style.width = '100%';
-            //     detailImgs[key].style.height = imgHeight.toString() + 'px';
-            // })
         }
         document.getElementsByClassName('section2')[0].style.height = backHeight.toString() + 'px';
         document.getElementsByClassName('section3')[0].style.paddingTop = (backHeight + 60).toString() + 'px';
+    }
+
+    imgWidth() {
+        // 动态计算背景大图宽度
+        let section2 = document.getElementsByClassName('section2')[0];
+        section2.style.width = document.getElementById('main').offsetWidth + 'px';
     }
 
 }
